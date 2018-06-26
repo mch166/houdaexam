f2e15ca5b3a482887bd1ac6ee80a223bf61b9f39
mch <mengchong@ehaier.com>
2018-06-27 00:54:03 +0800

    提交项目

---
+++ src/main/java/com/eliteams/quick4j/web/model/User.java
@@ -0,0 +1,98 @@
+package com.eliteams.quick4j.web.model;
+
+import java.util.Date;
+
+/**
+ * 用户模型
+ * 
+ * @author StarZou
+ * @since 2014年7月5日 下午12:07:20
+ **/
+public class User {
+    private Long id;
+
+    private String username;
+    
+    private String name;
+
+    private String password;
+
+    private String state;
+
+    private String createTime;
+    
+    private int type;
+
+    public User() {
+
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state == null ? null : state.trim();
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+    
+    
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	@Override
+    public String toString() {
+        return "User [id=" + id + ", username=" + username + ", password=" + password + ", state=" + state + ", createTime=" + createTime + "]";
+    }
+
+}
---
+++ src/test/java/com/eliteams/quick4j/test/service/UserServiceTest.java
@@ -0,0 +1,37 @@
+package com.eliteams.quick4j.test.service;
+
+import java.util.Date;
+import javax.annotation.Resource;
+
+import com.eliteams.quick4j.core.util.ApplicationUtils;
+import org.junit.Test;
+import com.eliteams.quick4j.core.feature.test.TestSupport;
+import com.eliteams.quick4j.web.model.User;
+import com.eliteams.quick4j.web.service.UserService;
+
+public class UserServiceTest extends TestSupport {
+
+    @Resource
+    private UserService userService;
+
+    @Test
+    public void test_insert() {
+        User model = new User();
+        model.setUsername("admin");
+        model.setPassword(ApplicationUtils.sha256Hex("admin"));
+        model.setCreateTime("2018-06-26");
+        userService.insert(model);
+    }
+
+    //@Test
+    public void test_10insert() {
+        for (int i = 0; i < 10; i++) {
+            User model = new User();
+            model.setUsername("starzou" + i);
+            model.setPassword(ApplicationUtils.sha256Hex("123456"));
+            model.setCreateTime("2018-06-26");
+            userService.insert(model);
+        }
+    }
+
+}
